bbb6316cdd309eb4b11bc0bc0974f5818132c093
FlawedCrystal <cargin.art@gmail.com>
2022-11-08 14:59:09 +0300

    main hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from 'react';
+
 import Header from './components/Header';
 import Book from './components/Book';
 
 function App() {
+
+  const [books, setBooks] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://6369798728cd16bba71e5865.mockapi.io/books')
+    .then((res) => (res.json()))
+    .then((json) => (setBooks(json)));
+  }, []);
+
   return (
     <div>
       <Header />
@@ -9,14 +20,11 @@ function App() {
         <div className='container'>
           <h2>Популярные книги</h2>
           <div className="books">
-            <Book title={'Старик и море. Зеленые холмы арфики'} author={'Эрнест Хемингуэй'} year={'1935'} pages={'310'} price={'199'} cover={'/img/books/The-Old-Man-and-the-Sea.jpg'} />
-            <Book title={'На западном фронте без перемен'} author={'Эрих Мария Ремарк'} year={'1929'} pages={'200'} price={'149'} cover={'/img/books/All-Quiet-on-the-Western-Front.jpg'} />
-            <Book title={'Великий Гэтсби'} author={'Фрэнсис Скотт Фицджеральд'} year={'1925'} pages={'180'} price={'99'} cover={'/img/books/The-Great-Gatsby.jpg'} />
-            <Book title={'Убить пересмешника'} author={'Харпер Ли'} year={'1960'} pages={'350'} price={'219'} cover={'/img/books/To-Kill-a-Mockingbird.jpg'} />
-            <Book title={'Преступление и наказание'} author={'Федор Достоевский'} year={'1866'} pages={'680'} price={'299'} cover={'/img/books/Crime-and-Punishment.jpg'} />
-            <Book title={'Цветы для Элджернона'} author={'Дэниел Киз'} year={'1966'} pages={'240'} price={'159'} cover={'/img/books/Flowers-for-Algernon.jpg'} />
-            <Book title={'Над пропастью во ржи'} author={'Дж. Д. Сэлинджер'} year={'1951'} pages={'240'} price={'199'} cover={'/img/books/The-Catcher-in-the-Rye.jpg'} />
-            <Book title={'Превращение (сборник)'} author={'Франц Кафка'} year={'1912'} pages={'416'} price={'179'} cover={'/img/books/Die-Verwandlung.jpg'} />
+            {
+              books.map((obj) => (
+                <Book title={obj.title} author={obj.author} year={obj.year} pages={obj.pages} price={obj.price} cover={obj.cover} />
+              ))
+            }
           </div>
         </div>
       </main>
--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -1,11 +1,19 @@
+import React from 'react';
+
 import styles from './Book.module.scss';
 
 function Book(props) {
+  const [like, setLike] = React.useState(false);
+
+  const clickLike = () => {
+    setLike(!like);
+  };
+
   return (
     <article className={styles.book}>
       <div className={styles.infoTop}>
         <span className={styles.price}>{props.price} &#8381;</span>
-        <img className={styles.like} src="/img/logo/like.svg" alt="like" />
+        <img onClick={() => clickLike()} className={styles.like} src={like ? "/img/logo/liked.svg" : "/img/logo/like.svg"} alt="like" />
       </div>
       <div className={styles.infoBottom}>
         <span className={styles.title}>{props.title}</span>
@@ -13,7 +21,7 @@ function Book(props) {
         <span className={styles.year}>{props.year}</span>
         <span className={styles.pages}>{props.pages} стр.</span>
       </div>
-      <img className={styles.bookCover} src={props.cover} alt="book" />
+      <img className={styles.bookCover} src={`/img/books/${props.cover}.jpg`} alt="book" />
     </article>
   );
 };
