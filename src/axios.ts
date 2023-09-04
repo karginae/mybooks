7c2ab9c65331d03de225c27f2f60a5a4c29ded24
karginae <cargin.art@gmail.com>
2023-09-04 23:32:17 +0300

    config axios

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:4444',
+  baseURL: 'http://45.12.73.185',
 });
 
 instance.interceptors.request.use((config) => {
